066049e13105b3acb85a296f99f1e2d95a89ea52
Vadim <v.marcenko@gmx.de>
2019-11-22 14:35:00 +0100

    Swagger Config for UI with Security

--- src/main/java/de/fzj/atlascore/configuration/SwaggerConfig.java
+++ src/main/java/de/fzj/atlascore/configuration/SwaggerConfig.java
@@ -2,11 +2,17 @@ package de.fzj.atlascore.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Configuration for the swagger documentation
@@ -15,12 +21,59 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(authorizationHeaderScheme()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Atlas Core REST API",
+                "Description of all endpoints provided by the atlas core",
+                "0.0.1",
+                "Terms of Service",
+                new Contact("Vadim Marcenko", "", "v.marcenko@fz-juelich.de"),
+                "License ...",
+                "License URL ...",
+                Collections.emptyList()
+        );
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(authorizationHeaderReference()))
+                .forPaths(PathSelectors.any())
                 .build();
     }
+
+    //region === Scheme and reference for setting Authorization header in swagger ui
+    private SecurityScheme authorizationHeaderScheme() {
+        return new ApiKey(AUTHORIZATION_HEADER, AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityReference authorizationHeaderReference() {
+        return new SecurityReference(AUTHORIZATION_HEADER, new AuthorizationScope[0]);
+    }
+    //endregion
+
+    //region === Scheme and reference for Basic Auth in swagger ui (NOT USED AT THE MOMENT)
+    /*private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
+    }
+    */
+    //endregion
+
 }
