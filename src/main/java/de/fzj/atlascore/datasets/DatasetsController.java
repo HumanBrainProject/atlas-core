e743b0cf9f8cc00c738d628f57a3da81b312ad5d
Vadim <v.marcenko@gmx.de>
2020-03-13 13:53:10 +0100

    Controller and Service for getting datasets

--- src/main/java/de/fzj/atlascore/datasets/DatasetsController.java
+++ src/main/java/de/fzj/atlascore/datasets/DatasetsController.java
@@ -3,6 +3,7 @@ package de.fzj.atlascore.datasets;
 import de.fzj.atlascore.configuration.ControllerPaths;
 import de.fzj.atlascore.knowledgegraph.KnowledgeGraphService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,24 +21,45 @@ public class DatasetsController {
     }
 
     @GetMapping(value = "/parcellations")
-    public Object getParcellationDatasets(@RequestParam(name = "id", required = false) String id) {
+    public Object getParcellationDatasets(
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationatlas/v1.0.0/..."
+            )
+            @RequestParam(name = "id", required = false) String id) {
         return knowledgeGraphService.getParcellationDatasets(id);
     }
 
     @GetMapping(value = "/referencespaces")
-    public Object getReferencespaceDatasets(@RequestParam(name = "id", required = false) String id) {
+    public Object getReferencespaceDatasets(
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/reference_space/v1.0.0/..."
+            )
+            @RequestParam(name = "id", required = false) String id) {
         return knowledgeGraphService.getReferencespaceDatasets(id);
     }
 
     @GetMapping(value = "/regions")
-    public Object getRegionsDatasets(@RequestParam(name = "id", required = false) String id) {
+    public Object getRegionsDatasets(
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationregion/v1.0.0/..."
+            )
+            @RequestParam(name = "id", required = false) String id) {
         return knowledgeGraphService.getRegionsDatasets(id);
     }
 
     @GetMapping
     public Object getAllDatasets(
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/reference_space/v1.0.0/..."
+            )
             @RequestParam(name = "referencespace", required = false) String referencespace,
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationatlas/v1.0.0/..."
+            )
             @RequestParam(name = "parcellation", required = false) String parcellation,
+            @ApiParam(name="id", value="KnowledgeGraph id https://schema.hbp.eu/myQuery/@id \n " +
+                    "Format: https://nexus.humanbrainproject.org/v0/data/minds/core/parcellationregion/v1.0.0/..."
+            )
             @RequestParam(name = "region", required = false) String region) {
         return knowledgeGraphService.getAllDatasets(referencespace, parcellation, region);
     }
